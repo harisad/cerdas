9e453b0f958ba59017ad7ef24cb1c02177c7487b
Haris Mujianto <aldousxin123@gmail.com>
2022-07-21 03:36:30 +0700

    telepot

--- app.py
+++ app.py
@@ -1,136 +1,100 @@
-from flask import Flask
-from flask import request
-from flask import Response
-import requests
-import random
-import json
+import sys
+import time
+import telepot
 
-TOKEN = "5324727239:AAH5ptASwVr2pUc4L8xI8gPcjBYGBBjYL-g"
-app = Flask(__name__)
-def parse_message(message):
-    print("message-->",message)
-    chat_id = message['message']['chat']['id']
-    txt = message['message']['text']
-    print("chat_id-->", chat_id)
-    print("txt-->", txt)
-    return chat_id,txt
- 
-def tel_send_message(chat_id, text):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    payload = {
-                'chat_id': chat_id,
-                'text': text
-                }
-   
-    r = requests.post(url,json=payload)
-    return r
-def tel_send_image(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    payload = {
-        'chat_id': chat_id,
-        'photo': "https://i.postimg.cc/vBtBjRvm/photo-6242397900453818906-y.jpg",
-        'caption': "Ya Jelas yang di foto ini"
-    }
- 
-    r = requests.post(url, json=payload)
-    return r
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.loop import MessageLoop
 
-def tel_send_poll(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
-    randnum = random.randint(0,4)
-    payload = {
-        'chat_id': chat_id,
-        "question": "Seberapa peduli kamu terhadap cinta?",
-        "options": json.dumps(["10%", "25%", "50%", "75%" , "100%"]),
-        "is_anonymous": False,
-        "type": "quiz",
-        "correct_option_id": randnum
-    }
- 
-    r = requests.post(url, json=payload)
- 
-    return r
+numbers={}
+cntr=0
+flagOfDo=0
+result=1
 
-def tel_send_poll2(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
-    randnum = random.randint(0,4)
-    payload = {
-        'chat_id': chat_id,
-        "question": "Seberapa cuek kamu?",
-        "options": json.dumps(["10%", "25%", "50%", "75%" , "100%"]),
-        "is_anonymous": False,
-        "type": "quiz",
-        "correct_option_id": randnum
-    }
- 
-    r = requests.post(url, json=payload)
- 
-    return r
+def handle(msg):
+    global flagOfLevel
+    
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='*', callback_data='2')],[InlineKeyboardButton(text='%', callback_data='1')],[InlineKeyboardButton(text='+', callback_data='3')],[InlineKeyboardButton(text='-', callback_data='4')]
+    ])
+    content_type, chat_type, chat_id = telepot.glance(msg)
+#    print(msg)
 
-def tel_send_image2(chat_id):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    payload = {
-        'chat_id': chat_id,
-        'photo': "https://i.postimg.cc/DwY47Ksp/4c9157ef-6d83-4446-a5fe-4b9a85e851bd.jpg",
-        'caption': "Ya Jelas yang di foto ini"
-    }
- 
-    r = requests.post(url, json=payload)
-    return r
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        msg = request.get_json()
-       
-        chat_id,txt = parse_message(msg)
-        if txt == "/help":
-            tel_send_message(chat_id,"COMMAND BOT \n Arsyila \n siapa wanita tercantik? \n pasangan terbaik? \n haris nyebelin? \n kamu itu apa? \n /polling \n /cuek \n /gombal")
-        if txt == "Tes":
-            g1 = tel_send_image(chat_id)
-            g2 = tel_send_poll(chat_id)
-            arg = [g1,g2]
-            random.choice(arg)
-
-        if txt == "kamu itu apa?":
-            robot = ["Aku bot dibuat Haris" , "Aku manusia sama sepertimu"]
-            tel_send_message(chat_id,random.choice(robot))
-
-        if txt == "cila sayang haris?":
-            syg = ["sayang bangeeeeeettttttt", "sayang dong masa enggak"]
-            tel_send_message(chat_id,random.choice(syg))
-
-        if txt == "haris nyebelin?":
-            nyebelin = ["nyebelin banget", "enggak sih", "nyebelin tapi cila sayang"]
-            tel_send_message(chat_id,random.choice(nyebelin))
-
-        if txt == "/polling":
-            tel_send_poll(chat_id)
-
-        if txt == "/cuek":
-            tel_send_poll2(chat_id)
-
-        if txt == "siapa wanita tercantik?":
-            tel_send_image(chat_id)
+    global flagOfDo
+    global cntr
+    global result
+    
+    if msg['text']=='/start':
+        bot.sendMessage(chat_id, 'hii ! it is a simple calculator with python.enter the first number.')
+        flagOfDo=1
+        cntr=0
+        return
         
-        if txt == "pasangan terbaik?":
-            tel_send_image2(chat_id)
+    numbers[cntr]=int(msg['text'])
+    
+    if cntr==1:
+        bot.sendMessage(chat_id,'choose your function',reply_markup=keyboard)
+        cntr=0
+        result=0
+    
+    if result==1:
+        if flagOfDo==1:
+            bot.sendMessage(chat_id,'Send your second number ...')
+            flagOfDo=0
+            cntr=1
+            return
+  
+        else:
+            bot.sendMessage(chat_id,'Send first number ...')
+            flagOfDo=1
+            return
+#     bot.sendMessage(chat_id, message)
 
-        if txt == "/gombal":
-            gombal = ["Seandainya aku ini gelas, aku pengen deh kamu yang jadi airnya. Soalnya cuma kamu yang bisa mengisi kekosongan hidup aku",
-                      "Aku punya kemoceng buat kamu nih. Buat bersihin hati kamu dari nama-nama cowok yang udah nyakitin kamu",
-                      "Kamu itu sama seperti kemerdekaan. Sama-sama harus diperjuangkan",
-                      "Semenjak lihat kamu aku tuh jadi sakit mata, karena semua yang aku lihat jadi hitam putih. Cuma kamu doang yang berwarna",
-                      "Meskipun kamu dari ujung lautan ngelempar senyum, tapi langsung masuk ke hati aku"]
 
-            tel_send_message(chat_id, random.choice(gombal))
 
-        if txt == "Arsyila":
-            syila = ["iya apa?","cila cantik","kesayangan haris", "apa si manggil manggil"]
-            tel_send_message(chat_id,random.choice(syila))      
-        return Response('ok', status=200)
+
+def on_callback_query(msg):
+	
+    query_id, from_id, query_data=telepot.glance(msg, flavor='callback_query')    
+    #    print(msg)
+    global result
+    global numbers
+
+    if query_data=='0':
+        bot.sendMessage(from_id,'Enter /start to start again',reply_markup=keyboard)
+        numbers={}
+        
+    elif query_data=='1':
+        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' % ' + str(numbers[1]) + '=' + str(numbers[0]/numbers[1]))
+        bot.sendMessage(from_id,'Enter /start to start again')
+        result=1
+        numbers={}
+        
+    elif query_data=='2':
+        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' * ' + str(numbers[1]) + '=' + str(numbers[0]*numbers[1]))
+        bot.sendMessage(from_id,'Enter /start to start again')
+        result=1
+        numbers={}
+        
+    elif query_data=='3':
+        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' + ' + str(numbers[1]) + '=' + str(numbers[0]+numbers[1]))
+        bot.sendMessage(from_id,'Enter /start to start again')
+        result=1
+        numbers={}
+        
+    elif query_data=='4':
+        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' - ' + str(numbers[1]) + '=' + str(numbers[0]-numbers[1]))
+        bot.sendMessage(from_id,'Enter /start to start again')
+        result=1
+        numbers={}
     else:
-        return "<h1>Welcome!</h1>"
- 
-if __name__ == '__main__':
-    app.run()
+        bot.sendMessage(from_id,'try again !!!')
+        result=1
+        bot.sendMessage(from_id,'Enter /start to start again')
+       	   
+bot = telepot.Bot('5324727239:AAH5ptASwVr2pUc4L8xI8gPcjBYGBBjYL-g')
+MessageLoop(bot, {'chat': handle,
+                  'callback_query': on_callback_query}).run_as_thread()
+print ('Listening ...')
+
+while 1:
+    time.sleep(10)
