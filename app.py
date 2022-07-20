df6c62227a973d313d2e3dd6ab507e0547c62d4d
Haris Mujianto <aldousxin123@gmail.com>
2022-07-21 03:52:29 +0700

    kalkulator

--- app.py
+++ app.py
@@ -1,100 +1,59 @@
-import sys
-import time
-import telepot
+# -*- coding: utf-8 -*-
 
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from telepot.loop import MessageLoop
+import os
+import re
 
-numbers={}
-cntr=0
-flagOfDo=0
-result=1
+from telegram.ext import Updater, CommandHandler
+import logging
 
-def handle(msg):
-    global flagOfLevel
-    
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='*', callback_data='2')],[InlineKeyboardButton(text='%', callback_data='1')],[InlineKeyboardButton(text='+', callback_data='3')],[InlineKeyboardButton(text='-', callback_data='4')]
-    ])
-    content_type, chat_type, chat_id = telepot.glance(msg)
-#    print(msg)
+token = os.environ['TELEGRAM_TOKEN']
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-    global flagOfDo
-    global cntr
-    global result
-    
-    if msg['text']=='/start':
-        bot.sendMessage(chat_id, 'hii ! it is a simple calculator with python.enter the first number.')
-        flagOfDo=1
-        cntr=0
-        return
-        
-    numbers[cntr]=int(msg['text'])
-    
-    if cntr==1:
-        bot.sendMessage(chat_id,'choose your function',reply_markup=keyboard)
-        cntr=0
-        result=0
-    
-    if result==1:
-        if flagOfDo==1:
-            bot.sendMessage(chat_id,'Send your second number ...')
-            flagOfDo=0
-            cntr=1
-            return
-  
-        else:
-            bot.sendMessage(chat_id,'Send first number ...')
-            flagOfDo=1
-            return
-#     bot.sendMessage(chat_id, message)
+logger = logging.getLogger(__name__)
 
 
-
-
-def on_callback_query(msg):
-	
-    query_id, from_id, query_data=telepot.glance(msg, flavor='callback_query')    
-    #    print(msg)
-    global result
-    global numbers
-
-    if query_data=='0':
-        bot.sendMessage(from_id,'Enter /start to start again',reply_markup=keyboard)
-        numbers={}
-        
-    elif query_data=='1':
-        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' % ' + str(numbers[1]) + '=' + str(numbers[0]/numbers[1]))
-        bot.sendMessage(from_id,'Enter /start to start again')
-        result=1
-        numbers={}
-        
-    elif query_data=='2':
-        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' * ' + str(numbers[1]) + '=' + str(numbers[0]*numbers[1]))
-        bot.sendMessage(from_id,'Enter /start to start again')
-        result=1
-        numbers={}
-        
-    elif query_data=='3':
-        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' + ' + str(numbers[1]) + '=' + str(numbers[0]+numbers[1]))
-        bot.sendMessage(from_id,'Enter /start to start again')
-        result=1
-        numbers={}
-        
-    elif query_data=='4':
-        bot.sendMessage(from_id,'your result: ' +  str(numbers[0]) + ' - ' + str(numbers[1]) + '=' + str(numbers[0]-numbers[1]))
-        bot.sendMessage(from_id,'Enter /start to start again')
-        result=1
-        numbers={}
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    exp = update.message.text.replace('/calc', '')
+    exp = "".join(exp.split())
+    if re.match('^([-+]?([(]?[0-9][)]?[+-/*]?))*$', exp):
+        try:
+            update.message.reply_text(eval(exp))
+        except:
+            update.message.reply_text('Ooops error saat kalkulasi')
     else:
-        bot.sendMessage(from_id,'try again !!!')
-        result=1
-        bot.sendMessage(from_id,'Enter /start to start again')
-       	   
-bot = telepot.Bot('5324727239:AAH5ptASwVr2pUc4L8xI8gPcjBYGBBjYL-g')
-MessageLoop(bot, {'chat': handle,
-                  'callback_query': on_callback_query}).run_as_thread()
-print ('Listening ...')
+        update.message.reply_text('Pola salah')
 
-while 1:
-    time.sleep(10)
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("calc", start))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
